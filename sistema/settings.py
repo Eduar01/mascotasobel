5804f67aea9431d66525daccfd81f6c4c019170b
Eduar01 <114963479+Eduar01@users.noreply.github.com>
2022-11-12 11:13:38 -0500

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'sistema.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': 'aplicacion\templates',
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
